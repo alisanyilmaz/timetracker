3d8d03cd9d0ac20426739b2dec5b5211931493f2
Alisan Yilmaz <alisanyilmaz@icloud.com>
2019-02-13 21:51:29 +0300

    Create Spring Boot application

---
+++ timetracker-bff/src/main/java/com/ayilmaz/timetracker/timetrackerbff/TimetrackerBffApplication.java
@@ -0,0 +1,14 @@
+package com.ayilmaz.timetracker.timetrackerbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetrackerBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetrackerBffApplication.class, args);
+	}
+
+}
+
---
+++ timetracker-bff/src/test/java/com/ayilmaz/timetracker/timetrackerbff/TimetrackerBffApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ayilmaz.timetracker.timetrackerbff;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TimetrackerBffApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
