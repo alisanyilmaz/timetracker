0e0cfbd964a4c63561cf75492845630804b1d250
Alisan Yilmaz <alisanyilmaz@icloud.com>
2019-02-16 11:38:55 +0300

    Create a class for app constants

---
+++ timetracker-bff/src/main/java/com/ayilmaz/timetracker/timetrackerbff/TimeTrackerConstants.java
@@ -0,0 +1,9 @@
+package com.ayilmaz.timetracker.timetrackerbff;
+
+public class TimeTrackerConstants {
+
+    public static final String PARAM_EMAIL = "email";
+    public static final String PARAM_OFFSET = "offset";
+    public static final String PARAM_LIMIT = "length";
+    
+}
